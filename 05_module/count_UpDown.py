215e823d4bac60d454878d87acd9883a4fc806b9
Hamberfim <adhamlingithub@gmail.com>
2019-06-05 15:40:27 -0500

    Started Module 5

---
+++ 05_module/count_UpDown.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 15:07:54 2019
+
+@author: adhamlin
+"""
+# Count up
+countUp = 1
+
+while countUp != 11:
+    print(countUp)
+    countUp = countUp + 1
+
+print("BOOM!")
+
+print("")  # added space between counts
+
+# Count Down
+countDown = 10
+
+while countDown != 0:
+    print(countDown)
+    countDown = countDown - 1
+
+print("Lift Off!")
