e3337654dc8b50056aa5bc0fe0305ab062f5b9b6
Hamberfim <adhamlingithub@gmail.com>
2019-07-16 11:16:05 -0500

    Finished Course

--- 014_module/Topic3/printLucky.java
+++
@@ -1,9 +0,0 @@
-public class printLucky {
-	public static void main(String[] args) {	
-		printNameLucky("Anthony Hamlin", 3);
-	  }
-	
-	public static void printNameLucky(String myName, int luckNum) {
-		System.out.println(myName + ", " + luckNum);
-	}
-}
